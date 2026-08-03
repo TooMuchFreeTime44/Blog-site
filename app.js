1a36d67e7faf3648718c068ffeae70ce943036e5
TooMuchFreeTime44 <jrkeliher@gmail.com>
2026-08-03 18:31:08 -0500

    Changed the wildcard path for the catch all route in app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.get('/api/post_list', async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'dist')));
 
-    app.get('*', (req, res) => {
+    app.get('/(.*)', (req, res) => {
         res.sendFile(path.join(__dirname, 'dist', 'index.html'));
     });
 }
